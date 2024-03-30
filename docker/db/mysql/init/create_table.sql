4a8c099ef0a9186fbf33beb2c26b568491219184
linirini <2001yerin@naver.com>
2024-03-31 02:26:58 +0900

    refactor : DDL 테이블 명 수정

--- docker/db/mysql/init/create_table.sql
+++ docker/db/mysql/init/create_table.sql
@@ -4,12 +4,12 @@
     PRIMARY KEY (user_id)
 );*/
 
-create table game(
-    game_id BIGINT NOT NULL AUTO_INCREMENT,
+create table room(
+    room_id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL,
     turn VARCHAR(64) NOT NULL,
     user_id BIGINT,
-    PRIMARY KEY (game_id)
+    PRIMARY KEY (room_id)
     /*FOREIGN KEY (user_id) references user (user_id)*/
 );
 
@@ -17,9 +17,9 @@ create table board(
     board_id BIGINT NOT NULL AUTO_INCREMENT,
     position VARCHAR(64) NOT NULL,
     piece_id BIGINT,
-    game_id BIGINT,
+    room_id BIGINT,
     PRIMARY KEY (board_id),
-    FOREIGN KEY (game_id) references game (game_id),
+    FOREIGN KEY (room_id) references room (room_id),
     FOREIGN KEY (piece_id) references piece (piece_id)
 );
 
