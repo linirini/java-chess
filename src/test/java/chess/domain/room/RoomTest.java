c41b9e8b69a5704aaf3a4927327352537e755b81
linirini <2001yerin@naver.com>
2024-04-01 06:13:41 +0900

    test : 방 이름 검증 테스트 추가

---
+++ src/test/java/chess/domain/room/RoomTest.java
@@ -0,0 +1,29 @@
+package chess.domain.room;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RoomTest {
+
+    @DisplayName("이름 길이가 1 이상, 10 이하이면 방이 생성된다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "aaaaaaaaaa"})
+    void createRoom(String name) {
+        //when & then
+        assertThatCode(() -> Room.of(name)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("이름 길이가 1 미만, 10 초과이면 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "aaaaaaaaaaa"})
+    void createInvalidRoom(String name) {
+        //when & then
+        assertThatThrownBy(() -> Room.of(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1자 이상, 10자 이하만 가능합니다.");
+    }
+}
